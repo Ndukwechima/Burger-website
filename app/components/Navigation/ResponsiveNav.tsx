87556d2b6b4645f1e7d8b530e205526902643e45
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-06-11 19:02:50 +0100

    Added completed header and with responsiveness

---
+++ app/components/Navigation/ResponsiveNav.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+import { useState } from "react";
+
+const ResponsiveNav = () => {
+  const [showNav, setShowNav] = useState(false);
+  const openNavHandler = () => setShowNav(true);
+  const closeNavHandler = () => setShowNav(false);
+
+  return (
+    <div>
+      <Nav openNav={openNavHandler} />
+      <MobileNav showNav={showNav} closeNav={closeNavHandler} />
+    </div>
+  );
+};
+
+export default ResponsiveNav;
